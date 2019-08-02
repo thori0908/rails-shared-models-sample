1257da70f4bc9622354194994bb07fa59111ac4d
thori0908 <mr.takumi.pc.0908@gmail.com>
2019-08-02 18:58:37 +0900

    add index, new pages of article

--- admin/app/controllers/articles_controller.rb
+++ admin/app/controllers/articles_controller.rb
@@ -69,6 +69,11 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.fetch(:article, {})
+      params
+        .require(:article)
+        .permit(
+          :title,
+          :body
+        )
     end
 end
