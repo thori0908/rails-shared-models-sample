fa2761aaf8ebe50549342640a32233808871d8c3
thori0908 <mr.takumi.pc.0908@gmail.com>
2019-08-05 18:02:37 +0900

    signup & login in web

---
+++ web/app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  skip_before_action :require_login, only: [:new, :create]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = login(params[:email], params[:password])
+
+    if @user.present?
+      redirect_to articles_path, notice: 'Login successful'
+    else
+      flash.now[:alert] = 'Login failed'
+      render action: 'new'
+    end
+  end
+
+  def destroy
+    logout
+  end
+end
