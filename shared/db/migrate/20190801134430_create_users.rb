f747e1ae3b2de9d66c6f3e29faa74a990d57b923
thori0908 <mr.takumi.pc.0908@gmail.com>
2019-08-05 14:20:51 +0900

    add column email password to users

--- shared/db/migrate/20190801134430_create_users.rb
+++ shared/db/migrate/20190801134430_create_users.rb
@@ -3,8 +3,13 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
+      t.string :email,            null: false
+      t.string :crypted_password
+      t.string :salt
 
       t.timestamps
     end
+
+    add_index :users, :email, unique: true
   end
 end
--- shared/db/schema.rb
+++ shared/db/schema.rb
@@ -22,8 +22,12 @@ ActiveRecord::Schema.define(version: 2019_08_02_092153) do
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
+    t.string "email", null: false
+    t.string "crypted_password"
+    t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
