c0251243a59e9bc74fe035b1102a08c48e9521c9
thori0908 <mr.takumi.pc.0908@gmail.com>
2019-08-05 16:10:21 +0900

    add login feature in admin

--- admin/config/routes.rb
+++ admin/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'login' => 'sessions#new', as: :login
+  post 'login' => 'sessions#create'
+  post 'logout' => 'sessions#destroy', as: :logout
+
   resources :articles
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- shared/app/models/admin_user.rb
+++ shared/app/models/admin_user.rb
@@ -1,4 +1,6 @@
 class AdminUser < ApplicationRecord
+  authenticates_with_sorcery!
+
   def full_name
     "#{last_name} #{first_name}"
   end
